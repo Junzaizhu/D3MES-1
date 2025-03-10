5cdb3959debd6f685dd1b1324fb39b8da710f7d1
Junzaizhu <104956018+Junzaizhu@users.noreply.github.com>
2025-03-10 14:42:01 +0800

    Update train_cla.py

--- train_cla.py
+++ train_cla.py
@@ -171,7 +171,7 @@ def main(args):
     #     transforms.ToTensor(),
     #     transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], inplace=True)
     # ])
-    data = np.load('merged_molecules.npz')
+    data = np.load('classchembl.npz')
     # 将数据加载为 data 类型
     x = torch.from_numpy(data['data']).to(torch.float).cuda()
     #x = x.transpose(0, 1).to(torch.float).cuda()
